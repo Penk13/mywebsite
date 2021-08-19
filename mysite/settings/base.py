ba950a90810809f76485caa86c23de1ac1c5a023
Penk13 <72376705+Penk13@users.noreply.github.com>
2021-08-19 22:28:47 +0700

    heroku - whitenoise

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -119,6 +119,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+STATIC_ROOT = BASE_DIR / "static-live" / "static-root"
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "static-live" / "media-root"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
